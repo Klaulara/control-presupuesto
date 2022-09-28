038ed37ff06be22276ac7090517097f380813df1
Claudia Lara <claudia_lara_zamora@hotmail.com>
2022-09-28 19:00:56 -0300

    Finishing modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import { useState } from 'react';
 import { Header } from './components/Header';
 import { Modal } from './components/Modal';
+import { generarId } from './helpers/functions';
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0);
-  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
-  const [modal, setModal] = useState(false)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+  const [gastos, setGastos] = useState([])
 
   const handleNuevoGasto = () => {
     setModal(true);
+
+    setTimeout(()=>{
+      setAnimarModal(true)
+    }, 500);
+  }
+
+  const guardarGasto = (gasto) => {
+    gasto.id = generarId();
+    setGastos([...gastos, gasto]);
+    
+    setAnimarModal(false);
+    setTimeout(()=>{
+      setModal(false);
+    }, 500);
   }
 
   return (
@@ -33,6 +50,9 @@ function App() {
       
       {modal && <Modal 
         setModal = {setModal}
+        animarModal = {animarModal}
+        setAnimarModal = {setAnimarModal}
+        guardarGasto = {guardarGasto}
       />}      
 
     </div>
